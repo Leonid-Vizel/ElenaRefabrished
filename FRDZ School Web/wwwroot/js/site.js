52ca074694d343ef7c83ec3ec236e93eb1733db6
Emil Maksudov <emil4ikmolor@gmail.com>
2023-03-29 02:45:25 +0300

    Улучшение дизайна

--- FRDZ School Web/wwwroot/js/site.js
+++ FRDZ School Web/wwwroot/js/site.js
@@ -16,3 +16,23 @@ let elements = document.querySelectorAll('.element-animation');
 for (let elm of elements) {
     observer.observe(elm);
 }
+
+/*=============================================================================================*/
+
+var heroShinker = function () {
+    var hero = $('.hero-nav'),
+        heroHeight = $('.hero-nav').outerHeight(true);
+    $(hero).parent().css('padding-top', heroHeight);
+    $(window).scroll(function () {
+        var scrollOffset = $(window).scrollTop();
+        if (scrollOffset < heroHeight) {
+            $(hero).css('height', (heroHeight - scrollOffset));
+        }
+        if (scrollOffset > (heroHeight - 215)) {
+            hero.addClass('fixme');
+        } else {
+            hero.removeClass('fixme');
+        };
+    });
+}
+heroShinker();
