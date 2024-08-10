c7bf2ca95c3bbf006deb506f4eb894b67bb7fcf9
Vizel Leonid <spartanlaska@gmail.com>
2024-08-10 22:43:59 +0300

    Renaming updates

--- FRDZSchool.Utility/SD.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FRDZSchool.Utility
-{
-    public static class SD
-    {
-        public const string Role_Admin = "Администратор";
-        public const string Role_Teacher = "Учитель";
-        public const string Role_Student = "Ученик";
-    }
-}
